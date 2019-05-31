bb81d2a62c559d005a3a05078867d6914bb05e70
mougithub19 <moubusiness18@gmail.com>
2019-05-31 22:31:42 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
+    <h3>{{title}}</h3>
     <h5>Display : {{display_data}}</h5>`,
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  title = 'Angular';
+  title = 'Angular Assignment';
   display_data = "Test Data";
+  display_data2:
   
 }
