8528de196c66086fe2f0ed4329c8545f3d358de5
mougithub19 <moubusiness18@gmail.com>
2019-06-02 11:50:26 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,18 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <h3>{{title}}</h3>
-    <p><input type = "text" [value]="display_data1" /></p>
-    <p>Display String  : {{display_data2}}</p>`,
+    <p>Property Binding : <input type = "text" [value]="display_data1" /></p>
+    <p>Event Binding : <input type = "text" (input)="display_data_fn($event.target.value)" /></p>
+    <p>Message : {{ display_data2 }} </p>`,
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
   title = 'Angular Assignment';
   display_data1:string = "Test Data";
-  display_data2:string;
-  constructor(){
-    this.display_data2="My name is Moupiya"
+  display_data2:string = "";
+  display_data_fn(val:string):void{
+    this.display_data2=val;
   }
+  constructor(){}
   
 }
