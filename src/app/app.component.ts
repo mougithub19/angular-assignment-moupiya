b61f5f80a2ab6b3a1b6cb9210b4167ff4632cf55
mougithub19 <moubusiness18@gmail.com>
2019-06-02 17:09:45 +0530

    3 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-    <h3>{{title}}</h3>
-    <div class="label_hd">Property Binding </div>
-    <div class="module"><input type = "text" [value]="display_data1" /></div>
-    <div class="label_hd">Event Binding </div>
-    <div class="module"><input type = "text" (input)="display_data_fn($event.target.value)" /></div>
-    <div class="label_hd">Message : {{ display_data2 }} </div>
-    <div class="label_hd">Two Way Data Binding </div>
-    <div class="module"><input type = "text" [(ngModel)]="display_data3" /></div>
-    <div class="label_hd">Message : {{ display_data3 }} </div>
-    <div class="label_hd">Class Binding </div>
-    <div class="module"><h3 [class.highlight]="chk">Test class binding</h3></div>
-    `,
+  templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  user_data:string[]=["User1","User2","User3","User4"];
+
+
+
+}
