bf57db33a68ed24d388084610f7a6223f1e0da11
mougithub19 <moubusiness18@gmail.com>
2019-06-02 11:39:18 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,16 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <h3>{{title}}</h3>
-    <p>Display : {{display_data}}</p>
+    <p><input type = "text" [value] = "display_data1" /></p>
     <p>Display String  : {{display_data2}}</p>`,
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
   title = 'Angular Assignment';
-  display_data = "Test Data";
+  display_data1:string = "Test Data";
   display_data2:string;
   constructor(){
-    this.display_data2="My name is Moupiya."
+    this.display_data2="My name is Moupiya"
   }
   
 }
