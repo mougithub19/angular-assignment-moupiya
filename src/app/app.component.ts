333267fdda486c886e28a35310d54e7d153611ad
mougithub19 <moubusiness18@gmail.com>
2019-06-02 14:25:01 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,8 @@ import { Component } from '@angular/core';
     <div class="label_hd">Two Way Data Binding </div>
     <div class="module"><input type = "text" [(ngModel)]="display_data3" /></div>
     <div class="label_hd">Message : {{ display_data3 }} </div>
+    <div class="label_hd">Class Binding </div>
+    <div class="module"><h3 [class.highlight]="chk">Test class binding</h3></div>
     `,
   styleUrls: [ './app.component.css' ]
 })
@@ -20,9 +22,11 @@ export class AppComponent  {
   display_data1:string = "Test Data";
   display_data2:string = "";
   display_data3:string = "Welcome";
+  chk:boolean=true;
   display_data_fn(val:string):void{
     this.display_data2=val;
   }
+
   constructor(){}
   
 }
