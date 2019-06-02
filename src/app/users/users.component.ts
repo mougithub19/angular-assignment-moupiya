25d27ae4fe3450a7a9b79e4b31ac3112e2b993ca
mougithub19 <moubusiness18@gmail.com>
2019-06-02 18:32:05 +0530

    3 files modified

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -16,9 +16,21 @@ export class UsersComponent implements OnInit {
       {
         "id":"1",
         "name":"User1",
-        "location":"User1",
+        "location":"location1",
 
-      }   
+      },
+      {
+        "id":"2",
+        "name":"User2",
+        "location":"location2",
+
+      },
+      {
+        "id":"3",
+        "name":"User3",
+        "location":"location3",
+
+      }         
     ];
 
 
